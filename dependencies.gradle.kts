a9947e27b633781b6b83b01f3c4d7b77ee29ea32
Šemsudin Tafilović <25798677+semsudin-tafilovic@users.noreply.github.com>
2025-04-15 17:47:59 +0200

    Updated dependencies (#14)

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -1,13 +1,13 @@
-apply plugin : 'com.android.library'
-
-ext {
-    mainLibraries = [
-        "androidx.core:core-ktx:1.12.0",
-        "androidx.appcompat:appcompat:1.6.1",
-        "com.google.android.material:material:1.11.0",
-        "androidx.recyclerview:recyclerview:1.3.2",
-        "androidx.lifecycle:lifecycle-runtime-ktx:2.7.0",
-        platform("com.google.firebase:firebase-bom:32.4.0"),
-        "com.google.firebase:firebase-messaging-ktx",
-    ]
-}
+//apply plugin : 'com.android.library'
+//
+//ext {
+//    mainLibraries = [
+//        "androidx.core:core-ktx:1.12.0",
+//        "androidx.appcompat:appcompat:1.6.1",
+//        "com.google.android.material:material:1.11.0",
+//        "androidx.recyclerview:recyclerview:1.3.2",
+//        "androidx.lifecycle:lifecycle-runtime-ktx:2.7.0",
+//        platform("com.google.firebase:firebase-bom:32.4.0"),
+//        "com.google.firebase:firebase-messaging-ktx",
+//    ]
+//}
