6a548d05161a2083f1f65c41c62cc29701632b13
tafilovic <sekitafilovic@gmail.com>
2025-12-23 22:44:05 +0100

    Improved logic for AppoxeeImpl, EngageApiImpl, AppoxeeAdapter.

--- mapp-engage-sdk/src/main/java/com/appoxee/internal/network/EngageApiImpl.kt
+++ mapp-engage-sdk/src/main/java/com/appoxee/internal/network/EngageApiImpl.kt
@@ -39,6 +39,7 @@ import com.appoxee.internal.network.response.BaseAdapter
 import com.appoxee.internal.network.response.InappAdapter
 import com.appoxee.internal.network.response.InboxAdapter
 import com.appoxee.internal.network.response.Response
+import com.appoxee.internal.network.response.ResponseAdapter
 import com.appoxee.internal.network.response.StatusAdapter
 import com.appoxee.internal.provider.DeviceProvider
 import com.appoxee.internal.storage.Storage
@@ -56,9 +57,8 @@ internal class EngageApiImpl(
     private val inboxPathV5 = "api/v5/device/inapp/inbox"
     private val inappPathV5 = "api/v5/device/nativeinapp"
     private val inappEventsPathV5 = "api/v5/device/inapp/tracking"
-    private val pushEventsPath = "/api/push/event"
+    private val pushEventsPath = "api/push/event"
 
-    //private val sdkKeyHeader = mapOf("X_KEY" to options.sdkKey)
     private val uniqueDeviceId = deviceProvider.getUniqueDeviceId()
 
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
@@ -76,79 +76,100 @@ internal class EngageApiImpl(
         return storage.getInitOptions()?.tenantId ?: throw DeviceNotRegisteredException()
     }
 
+    private suspend fun getDeviceOrError(): DevicePayload {
+        return storage.getDevicePayload() ?: throw DeviceNotRegisteredException()
+    }
+
+    private suspend fun createMessageBody(
+        eventName: String,
+        device: DevicePayload
+    ): MessageBody {
+        return MessageBody(
+            timestamp = Date().time,
+            id = UUID.randomUUID().toString(),
+            userId = device.dmcUserId ?: "",
+            alias = device.alias ?: "",
+            eventKey = eventName,
+            deviceId = device.udidHashed ?: ""
+        )
+    }
+
+    private suspend fun getCepHeaders(): Map<String, String> {
+        return mapOf(
+            "tenant_id" to getTenantId(),
+            "app_id" to getAppId()
+        )
+    }
+
+    private suspend fun <T> executeDevicePutRequest(
+        actions: NetworkData,
+        alias: String? = null,
+        adapter: ResponseAdapter<T>,
+        pathType: Request.PathType = Request.PathType.BASE
+    ): Response<T> {
+        val requestBody = RequestBody(
+            key = uniqueDeviceId,
+            actions = actions,
+            alias = alias
+        )
+        val request = Request.Put(path = devicePathV3, requestBody = requestBody)
+            .addHeader(getSdkKeyHeader())
+            .setPathType(pathType)
+        return networkClient.execute(request, adapter)
+    }
+
+    private suspend fun executeDefaultResponseRequest(
+        actions: NetworkData,
+        alias: String? = null,
+        pathType: Request.PathType = Request.PathType.BASE
+    ): Response<ResponseData<DefaultResponse>> {
+        return executeDevicePutRequest(
+            actions = actions,
+            alias = alias,
+            adapter = BaseAdapter { DefaultResponse.fromJSON(it) },
+            pathType = pathType
+        )
+    }
+
     override suspend fun registerDevice(
         register: RegisterDevice
     ): Response<ResponseData<RegisterPayload>> {
-        val deviceModel = RequestBody(key = uniqueDeviceId, actions = register)
-
-        val request = Request
-            .Put(path = devicePathV3, requestBody = deviceModel)
-            .addHeader(getSdkKeyHeader())
-
-        val response = networkClient.execute(request, BaseAdapter {
-            RegisterPayload.fromJSON(it)
-        })
-
-        return response
+        return executeDevicePutRequest(
+            actions = register,
+            adapter = BaseAdapter { RegisterPayload.fromJSON(it) }
+        )
     }
 
     override suspend fun updateDevice(alias: String, updateDevice: UpdateDevice): Response<ResponseData<DefaultResponse>> {
-        val updateModel = RequestBody(key = uniqueDeviceId, actions = updateDevice, alias = alias)
-        val request = Request.Put(path = devicePathV3, requestBody = updateModel)
-            .addHeader(getSdkKeyHeader())
-
-        val response = networkClient.execute(request, BaseAdapter {
-            DefaultResponse.fromJSON(it)
-        })
-
-        return response
+        return executeDefaultResponseRequest(
+            actions = updateDevice,
+            alias = alias
+        )
     }
 
     override suspend fun getDevice(): Response<ResponseData<DevicePayload>> {
-        val requestBody = RequestBody(key = uniqueDeviceId, actions = GetDevice())
-        val request = Request
-            .Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-
-        val response = networkClient.execute(request, BaseAdapter {
-            DevicePayload.fromJSON(it.getJSONObject("get"))
-        })
-
-        return response
+        return executeDevicePutRequest(
+            actions = GetDevice(),
+            adapter = BaseAdapter { DevicePayload.fromJSON(it.getJSONObject("get")) }
+        )
     }
 
     override suspend fun activate(timeSpent: Long): Response<ResponseData<DefaultResponse>> {
         val alias = storage.getDevicePayload()?.alias
             ?: return Response.error(DeviceNotRegisteredException())
 
-        val requestBody =
-            RequestBody(key = uniqueDeviceId, actions = Activation(timeSpent), alias = alias)
-
-        val request = Request
-            .Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-
-        val response = networkClient.execute(request, BaseAdapter {
-            DefaultResponse.fromJSON(it)
-        })
-
-        return response
+        return executeDefaultResponseRequest(
+            actions = Activation(timeSpent),
+            alias = alias
+        )
     }
 
     override suspend fun setAlias(
         alias: String,
     ): Response<ResponseData<DefaultResponse>> {
-        val requestBody = RequestBody(key = uniqueDeviceId, actions = SetAlias(alias))
-
-        val request = Request
-            .Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-
-        val adapter = BaseAdapter { DefaultResponse.fromJSON(json = it) }
-
-        val response = networkClient.execute(request, adapter)
-
-        return response
+        return executeDefaultResponseRequest(
+            actions = SetAlias(alias)
+        )
     }
 
     override suspend fun getAlias(): Response<ResponseData<DevicePayload>> {
@@ -156,151 +177,90 @@ internal class EngageApiImpl(
     }
 
     override suspend fun optIn(pushToken: String): Response<ResponseData<DefaultResponse>> {
-        val requestBody =
-            RequestBody(key = uniqueDeviceId, actions = OptIn(pushToken))
-
-        val request = Request
-            .Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-
-        val response = networkClient.execute(request, BaseAdapter {
-            DefaultResponse.fromJSON(it)
-        })
-        return response
+        return executeDefaultResponseRequest(
+            actions = OptIn(pushToken)
+        )
     }
 
     override suspend fun optOut(
         pushTokenBk: String
     ): Response<ResponseData<DefaultResponse>> {
-        val requestBody =
-            RequestBody(key = uniqueDeviceId, actions = OptOut(pushTokenBk))
-
-        val request = Request
-            .Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-
-        val response = networkClient.execute(request, BaseAdapter {
-            DefaultResponse.fromJSON(it)
-        })
-        return response
+        return executeDefaultResponseRequest(
+            actions = OptOut(pushTokenBk)
+        )
     }
 
     override suspend fun getAppConfig(): Response<ResponseData<AppConfigPayload>> {
-        val requestBody = RequestBody(key = uniqueDeviceId, actions = GetAppConfig())
-
-        val request = Request
-            .Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-
-        val response = networkClient.execute(request, BaseAdapter {
-            AppConfigPayload.fromJson(it.getJSONObject("app_conf"))
-        })
-
-        return response
+        return executeDevicePutRequest(
+            actions = GetAppConfig(),
+            adapter = BaseAdapter { AppConfigPayload.fromJson(it.getJSONObject("app_conf")) }
+        )
     }
 
     override suspend fun fetchInboxMessages(eventName: String): Response<InboxMessagesResponse> {
-        val device =
-            storage.getDevicePayload() ?: return Response.error(DeviceNotRegisteredException())
-        val requestBody =
-            MessageBody(
-                timestamp = Date().time,
-                id = UUID.randomUUID().toString(),
-                userId = device.dmcUserId ?: "",
-                alias = device.alias ?: "",
-                eventKey = eventName,
-                deviceId = device.udidHashed ?: "",
-            )
+        val device = try {
+            getDeviceOrError()
+        } catch (e: DeviceNotRegisteredException) {
+            return Response.error(e)
+        }
+
+        val requestBody = createMessageBody(eventName, device)
 
         val request = Request.Post(path = inboxPathV5, requestBody = requestBody)
             .setPathType(Request.PathType.CEP)
-            .addHeader(mapOf("tenant_id" to getTenantId()))
-            .addHeader(mapOf("app_id" to getAppId()))
+            .addHeader(getCepHeaders())
 
         return networkClient.execute(request, InboxAdapter(eventKey = eventName))
     }
 
     override suspend fun fetchInApp(eventName: String): Response<InappResponse> {
-        val device =
-            storage.getDevicePayload() ?: return Response.error(DeviceNotRegisteredException())
+        val device = try {
+            getDeviceOrError()
+        } catch (e: DeviceNotRegisteredException) {
+            return Response.error(e)
+        }
 
-        val requestBody =
-            MessageBody(
-                timestamp = Date().time,
-                id = UUID.randomUUID().toString(),
-                userId = device.dmcUserId ?: "",
-                alias = device.alias ?: "",
-                eventKey = eventName,
-                deviceId = device.udidHashed ?: ""
-            )
+        val requestBody = createMessageBody(eventName, device)
 
         val request = Request.Post(path = inappPathV5, requestBody = requestBody)
             .setPathType(Request.PathType.CEP)
-            .addHeader(mapOf("tenant_id" to getTenantId()))
-            .addHeader(mapOf("app_id" to getAppId()))
+            .addHeader(getCepHeaders())
 
         return networkClient.execute(request, InappAdapter())
     }
 
     override suspend fun addTags(tags: List<String>): Response<ResponseData<DefaultResponse>> {
-        val requestBody = RequestBody(key = uniqueDeviceId, Tags(tags, TagsAction.SET))
-        val request = Request
-            .Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-
-        val response = networkClient.execute(request, BaseAdapter {
-            DefaultResponse.fromJSON(it)
-        })
-
-        return response
+        return executeDefaultResponseRequest(
+            actions = Tags(tags, TagsAction.SET)
+        )
     }
 
     override suspend fun removeTags(tags: List<String>): Response<ResponseData<DefaultResponse>> {
-        val requestBody = RequestBody(key = uniqueDeviceId, Tags(tags, TagsAction.REMOVE))
-        val request = Request
-            .Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-
-        val response = networkClient.execute(request, BaseAdapter {
-            DefaultResponse.fromJSON(it)
-        })
-
-        return response
+        return executeDefaultResponseRequest(
+            actions = Tags(tags, TagsAction.REMOVE)
+        )
     }
 
     override suspend fun addCustomAttributes(attributes: Map<String, Any?>): Response<ResponseData<DefaultResponse>> {
-        val alias = storage.getDevicePayload()?.alias ?: return Response.error(
-            DeviceNotRegisteredException()
+        val alias = storage.getDevicePayload()?.alias
+            ?: return Response.error(DeviceNotRegisteredException())
+
+        return executeDefaultResponseRequest(
+            actions = SetAttributes(attributes = attributes),
+            alias = alias,
+            pathType = Request.PathType.BASE
         )
-        val attributeSet = SetAttributes(attributes = attributes)
-        val requestBody = RequestBody(key = uniqueDeviceId, alias = alias, actions = attributeSet)
-        val request = Request
-            .Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-            .setPathType(Request.PathType.BASE)
-
-        val response = networkClient.execute(request, BaseAdapter {
-            DefaultResponse.fromJSON(it)
-        })
-
-        return response
     }
 
     override suspend fun getCustomAttributes(attributes: List<String>): Response<ResponseData<Map<String, Any?>>> {
-        val alias = storage.getDevicePayload()?.alias ?: return Response.error(
-            DeviceNotRegisteredException()
+        val alias = storage.getDevicePayload()?.alias
+            ?: return Response.error(DeviceNotRegisteredException())
+
+        return executeDevicePutRequest(
+            actions = GetAttributes(attributes = attributes),
+            alias = alias,
+            adapter = BaseAdapter { CustomAttributesPayload.fromJson(it) }
         )
-        val attributeGet = GetAttributes(attributes = attributes)
-        val requestBody = RequestBody(key = uniqueDeviceId, alias = alias, actions = attributeGet)
-        val request = Request
-            .Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-
-        val response = networkClient.execute(request, BaseAdapter {
-            CustomAttributesPayload.fromJson(it)
-        })
-
-        return response
     }
 
     override suspend fun inappEvent(
@@ -309,21 +269,19 @@ internal class EngageApiImpl(
         trackingKey: TrackingKey,
         trackingAttributes: Map<String, *>
     ): Response<ResponseData<Boolean>> {
-        val device =
-            storage.getDevicePayload() ?: return Response.error(DeviceNotRegisteredException())
+        val device = try {
+            getDeviceOrError()
+        } catch (e: DeviceNotRegisteredException) {
+            return Response.error(e)
+        }
 
-        val messageContext =
-            MessageContext(originalEventId = originalEventId, templateId = templateId)
-
+        val messageContext = MessageContext(originalEventId = originalEventId, templateId = templateId)
         val tracking = Tracking(trackingKey = trackingKey, trackingAttributes = trackingAttributes)
-
-        val requestBody =
-            InappEvent(device = device, messageContext = messageContext, tracking = tracking)
+        val requestBody = InappEvent(device = device, messageContext = messageContext, tracking = tracking)
 
         val request = Request.Post(path = inappEventsPathV5, requestBody = requestBody)
             .setPathType(Request.PathType.CEP)
-            .addHeader(mapOf("tenant_id" to getTenantId()))
-            .addHeader(mapOf("app_id" to getAppId()))
+            .addHeader(getCepHeaders())
 
         return networkClient.execute(request, StatusAdapter())
     }
@@ -364,24 +322,14 @@ internal class EngageApiImpl(
             ?: return Response.error(DeviceNotRegisteredException())
 
         val appId = getAppId().toLongOrNull() ?: 0L
-
         val getRegions = GetRegions(lat, lng, version, appId, pageSize)
 
-        val requestBody = RequestBody(
-            key = uniqueDeviceId,
+        return executeDevicePutRequest(
+            actions = getRegions,
             alias = alias,
-            actions = getRegions
+            adapter = BaseAdapter { RegionsResponse.fromJSON(it) },
+            pathType = Request.PathType.BASE
         )
-
-        val request = Request.Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-            .setPathType(Request.PathType.BASE)
-
-        val response = networkClient.execute(request = request, adapter = BaseAdapter {
-            RegionsResponse.fromJSON(it)
-        })
-
-        return response
     }
 
     override suspend fun regionEvent(
@@ -391,11 +339,14 @@ internal class EngageApiImpl(
         regionId: Long,
         version: Int
     ): Response<ResponseData<DefaultResponse>> {
-        val device =
-            storage.getDevicePayload() ?: return Response.error(DeviceNotRegisteredException())
+        val device = try {
+            getDeviceOrError()
+        } catch (e: DeviceNotRegisteredException) {
+            return Response.error(e)
+        }
 
-        val dmcUserId: String =
-            device.dmcUserId ?: return Response.error(DeviceNotRegisteredException())
+        val dmcUserId = device.dmcUserId
+            ?: return Response.error(DeviceNotRegisteredException())
 
         val regionStatus = RegionStatus(
             timestamp = Date().time,
@@ -409,16 +360,9 @@ internal class EngageApiImpl(
             applicationId = getAppId()
         )
 
-        val requestBody = RequestBody(key = uniqueDeviceId, actions = regionStatus)
-
-        val request = Request.Put(path = devicePathV3, requestBody = requestBody)
-            .addHeader(getSdkKeyHeader())
-            .setPathType(Request.PathType.BASE)
-
-        val response = networkClient.execute(request, BaseAdapter {
-            DefaultResponse.fromJSON(it)
-        })
-
-        return response
+        return executeDefaultResponseRequest(
+            actions = regionStatus,
+            pathType = Request.PathType.BASE
+        )
     }
 }
