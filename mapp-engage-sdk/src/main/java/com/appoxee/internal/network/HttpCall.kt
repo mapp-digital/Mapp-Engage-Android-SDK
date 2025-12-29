9bb3df0e6d1852d49080cf013bcb1c40ec947ed6
tafilovic <sekitafilovic@gmail.com>
2025-12-29 21:47:24 +0100

    Updated unit tests

--- mapp-engage-sdk/src/main/java/com/appoxee/internal/network/HttpCall.kt
+++ mapp-engage-sdk/src/main/java/com/appoxee/internal/network/HttpCall.kt
@@ -8,42 +8,41 @@ import com.appoxee.shared.MappResult
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
+import java.util.concurrent.atomic.AtomicBoolean
 
 internal class HttpCall<T>(
     private val scope: CoroutineScope,
     private val call: suspend () -> T,
     private val dispatchersProvider: DispatchersProvider
 ) : Call<T> {
-    private val mutex = Mutex()
+    private val executed = AtomicBoolean(false)
 
-    @Volatile
-    private var executed: Boolean = false
-
-    private fun isExecuted(): Boolean {
-        return runBlocking {
-            mutex.withLock {
-                return@runBlocking executed
-            }
-        }
+    private fun markAsExecuted(): Boolean {
+        return !executed.compareAndSet(false, true)
     }
 
     override fun execute(): MappResult<T> = runBlocking {
-        if (isExecuted()) throw CallConsumedException()
+        if (markAsExecuted()) throw CallConsumedException()
         executeWithErrorHandling()
     }
 
     override suspend fun asSuspend(): MappResult<T> {
-        if (isExecuted()) throw CallConsumedException()
+        if (markAsExecuted()) throw CallConsumedException()
         return withContext(dispatchersProvider.defaultDispatcher) {
             executeWithErrorHandling()
         }
     }
 
     override fun enqueue(callback: MappCallback<T>?) {
-        if (isExecuted()) throw CallConsumedException()
+        if (markAsExecuted()) {
+            scope.launch {
+                withContext(dispatchersProvider.mainDispatcher) {
+                    callback?.onResult(MappResult.Error(CallConsumedException()))
+                }
+            }
+            return
+        }
         scope.launch {
             val result = withContext(dispatchersProvider.defaultDispatcher) {
                 executeWithErrorHandling()
@@ -56,7 +55,6 @@ internal class HttpCall<T>(
 
     private suspend fun executeWithErrorHandling(): MappResult<T> {
         return try {
-            executed = true
             val result = call.invoke()
             MappResult.Success(result)
         } catch (e: Throwable) {
