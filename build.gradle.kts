59c178310897cb31cc4c5227a5c225219a3aac13
tafilovic <sekitafilovic@gmail.com>
2026-03-04 23:15:20 +0100

    Updated publishing plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        maven("https://jitpack.io")
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.github.tafilovic:central-portal-publisher:2.0.7")
-    }
-}
-
 plugins {
     id("com.android.application") version "8.13.2" apply false
     id("com.android.library") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
+    id("io.github.tafilovic.central-portal-publisher") version "2.0.11" apply false
+}
+
+gradle.projectsEvaluated {
+    tasks.withType<JavaCompile>().configureEach {
+        options.compilerArgs.addAll(listOf("-Xlint:deprecation", "-Xlint:unchecked"))
+    }
 }
