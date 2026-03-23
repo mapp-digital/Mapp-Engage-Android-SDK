69254967e308b6a9922eb3e483291acbae4d3ebb
tafilovic <sekitafilovic@gmail.com>
2026-03-24 00:52:35 +0100

    update: kotlin and publishing plugins versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application") version "9.1.0" apply false
     id("com.android.library") version "9.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
-    id("io.github.tafilovic.central-portal-publisher") version "2.0.12" apply false
+    id("io.github.tafilovic.central-portal-publisher") version "2.0.13" apply false
 }
 
 gradle.projectsEvaluated {
